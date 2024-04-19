88dc1fbd33be204bc162db9480f50e350758a125
Maykon07 <maykonsantos215@gmail.com>
2024-04-19 13:40:00 -0300

    parte 2

--- medico/models.py
+++ medico/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 def is_medico(user):
@@ -31,6 +32,12 @@ class DadosMedico(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    @property
+    def proxima_data(self):
+        proxima_data = DatasAbertas.objects.filter(user=self.user).filter(data__gt=datetime.now()).filter(agendado=False).order_by('data').first()
+
+        return proxima_data
 
 
 class DatasAbertas(models.Model):
